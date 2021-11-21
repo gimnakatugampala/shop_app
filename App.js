14bfb5f68f7614b1dd6fe806d9b94c9db8410bb0
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-11-21 18:08:28 +0530

    redux

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore , combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products:productsReducer
+})
+
+
+const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Provider store={store}>
+     <View></View>
+   </Provider>
   );
 }
 
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View , FlatList } from 'react-native'
+
+export default function ProductsOverviewScreen() {
+    return (
+        <FlatList />
+    )
+}
+
+const styles = StyleSheet.create({})
